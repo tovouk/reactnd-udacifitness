2cda458b31b1f3820ccafac358c9c8cc37ab641f
Hectic Envek <burningremedy@gmail.com>
2020-07-14 19:39:52 -0500

    Add util files and debugger

--- App.js
+++ App.js
@@ -2,18 +2,26 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import LotsOfGreetings from './Greetings'
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  componentDidMount(){
+    console.log("Before")
+    debugger
+    console.log("After")
+  }
+
+  render(){
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    )
+  }
 }
