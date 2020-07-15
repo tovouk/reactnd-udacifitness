33186bf2db0a10892dacd838e004ba167b77b36b
Hectic Envek <burningremedy@gmail.com>
2020-07-15 18:32:06 -0500

    Create new component and metadeta helper func

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { View, Text } from "react-native";
-import {Ionicons} from '@expo/vector-icons'
-import LotsOfGreetings from './Greetings'
+import { View } from "react-native";
+import AddEntry from './components/AddEntry'
+
 
 export default class App extends React.Component {
   componentDidMount(){
@@ -19,7 +19,7 @@ export default class App extends React.Component {
           alignItems: "center",
         }}
       >
-        <Ionicons name='ios-pizza' color='red' size={100}/>
+        <AddEntry />
       </View>
     )
   }
