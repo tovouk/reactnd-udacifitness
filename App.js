ef81eb31dd7a9f34a808e632456acb8b32de952c
Hectic Envek <burningremedy@gmail.com>
2020-07-16 22:03:29 -0500

    Fix reducer and add redux to app

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { View,Text, TouchableHighlight,StyleSheet,TouchableNativeFeedback,TouchableOpacity,TouchableWithoutFeedback } from "react-native";
 import AddEntry from "./components/AddEntry";
-
+import {createStore} from "redux"
+import {Provider} from "react-redux"
+import reducer from './reducers'
 
 export default class App extends React.Component {
   handlePress = () => {
@@ -10,10 +12,12 @@ export default class App extends React.Component {
 
   render(){
     return (
-      <View
-        style={style.container}>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View
+          style={style.container}>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
--- reducers/index.js
+++ reducers/index.js
@@ -1,16 +1,16 @@
 import {RECEIVE_ENTRIES,ADD_ENTRY} from '../actions'
 
-export function entries (state = {}, action) {
+export default function entries (state = {}, action) {
     switch(action.type){
         case RECEIVE_ENTRIES:
             return {
                 ...state,
-                action.entries
+                ...action.entries
             }
         case ADD_ENTRY:
             return {
                 ...state,
-                action.entry
+                ...action.entry
             }
         default:
             return state
